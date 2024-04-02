6223b9b34a4342fb90af04249ab4c9ceb044e82f
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-04-02 10:37:46 +0200

    Done

--- src/script.js
+++ src/script.js
@@ -1,23 +1,26 @@
 function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
-    
-    if (taskInput.value !== '') {
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(taskInput.value));
-      li.onclick = function() {
-        this.classList.toggle('completed');
-      };
-      
-      var deleteButton = document.createElement('button');
-      deleteButton.appendChild(document.createTextNode('Delete'));
-      deleteButton.onclick = function() {
-        taskList.removeChild(li);
-      };
-      
-      li.appendChild(deleteButton);
-      taskList.appendChild(li);
-      taskInput.value = '';
-    }
-  }
+  var taskInput = document.getElementById('taskInput');
+  var taskList = document.getElementById('taskList');
   
+  if (taskInput.value !== '') {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(taskInput.value));
+    
+    var completeButton = document.createElement('button');
+    completeButton.appendChild(document.createTextNode('Completed'));
+    completeButton.onclick = function() {
+      li.classList.toggle('completed');
+    };
+    
+    var deleteButton = document.createElement('button');
+    deleteButton.appendChild(document.createTextNode('Delete'));
+    deleteButton.onclick = function() {
+      taskList.removeChild(li);
+    };
+    
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+    taskInput.value = '';
+  }
+}
